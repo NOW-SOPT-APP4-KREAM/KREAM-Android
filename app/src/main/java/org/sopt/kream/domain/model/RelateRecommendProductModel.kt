3e8ae60f11f9be0b87ad92b40a1aa6c0acab76e1
jihyunniiii <jita1125@naver.com>
2024-05-20 23:28:16 +0900

    [feat] #20 검색 뷰 모델

---
+++ app/src/main/java/org/sopt/kream/domain/model/RelateRecommendProductModel.kt
@@ -0,0 +1,9 @@
+package org.sopt.kream.domain.model
+
+data class RelateRecommendProductModel(
+    val thumbnailUrl: String,
+    val engTitle: String,
+    val price: String,
+    val isFast: Boolean,
+    val scrapCount: String
+)
---
+++ app/src/main/java/org/sopt/kream/domain/model/SearchFindProductModel.kt
@@ -0,0 +1,13 @@
+package org.sopt.kream.domain.model
+
+data class SearchFindProductModel(
+    val thumbnailUrl: String,
+    val brandTitle: String,
+    val engTitle: String,
+    val title: String,
+    val price: String,
+    val transactionCount: String,
+    val scrapCount: String,
+    val styleCount: String,
+    val isFast: Boolean
+)
