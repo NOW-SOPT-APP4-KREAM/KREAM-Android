0e39b30d1eb69647fd2cea9ab30c15fee152da0d
Hyobeen-Park <amanda0109@naver.com>
2024-05-22 03:16:58 +0900

    [chore] #22 Just Dropped 아이템 레이아웃 및 네이밍 수정

--- app/src/main/java/org/sopt/kream/presentation/ui/main/home/recommend/RecommendJustDroppedAdapter.kt
+++ app/src/main/java/org/sopt/kream/presentation/ui/main/home/recommend/RecommendJustDroppedAdapter.kt
@@ -3,38 +3,41 @@ package org.sopt.kream.presentation.ui.main.home.recommend
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
-import org.sopt.kream.databinding.ItemRecommendReleaseProductBinding
+import org.sopt.kream.databinding.ItemRecommendJustDroppedProductBinding
 import org.sopt.kream.domain.model.RecommendJustDroppedProductModel
 import org.sopt.kream.util.view.ItemDiffCallback
 
 class RecommendJustDroppedAdapter(
     private val navigateToProductDetail: (Int) -> Unit,
-) : ListAdapter<RecommendJustDroppedProductModel,
-        RecommendJustDroppedViewHolder>(
-    ItemDiffCallback<RecommendJustDroppedProductModel>(
-        onContentsTheSame = { old, new -> old == new },
-        onItemsTheSame = { old, new -> old.engTitle == new.engTitle }
-    ),
-) {
+) : ListAdapter<
+        RecommendJustDroppedProductModel,
+        RecommendJustDroppedViewHolder,
+        >(
+        ItemDiffCallback<RecommendJustDroppedProductModel>(
+            onContentsTheSame = { old, new -> old == new },
+            onItemsTheSame = { old, new -> old.engTitle == new.engTitle },
+        ),
+    ) {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
-    ): RecommendJustDroppedViewHolder = RecommendJustDroppedViewHolder(
-        ItemRecommendReleaseProductBinding.inflate(
-            LayoutInflater.from(parent.context),
-            parent,
-            false,
-        ),
-        navigateToProductDetail,
-    )
+    ): RecommendJustDroppedViewHolder =
+        RecommendJustDroppedViewHolder(
+            ItemRecommendJustDroppedProductBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false,
+            ),
+            navigateToProductDetail,
+        )
 
     override fun onBindViewHolder(
         holder: RecommendJustDroppedViewHolder,
         position: Int,
     ) {
         holder.onBind(
-            recommendJustDroppedProductModel =
-            currentList[position], position = position
+            recommendJustDroppedProductModel = currentList[position],
+            position = position,
         )
     }
 }
--- app/src/main/java/org/sopt/kream/presentation/ui/main/home/recommend/RecommendJustDroppedViewHolder.kt
+++ app/src/main/java/org/sopt/kream/presentation/ui/main/home/recommend/RecommendJustDroppedViewHolder.kt
@@ -1,30 +1,53 @@
 package org.sopt.kream.presentation.ui.main.home.recommend
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import org.sopt.kream.R
-import org.sopt.kream.databinding.ItemRecommendReleaseProductBinding
+import org.sopt.kream.databinding.ItemRecommendJustDroppedProductBinding
 import org.sopt.kream.domain.model.RecommendJustDroppedProductModel
 
 class RecommendJustDroppedViewHolder(
-    private val binding: ItemRecommendReleaseProductBinding,
+    private val binding: ItemRecommendJustDroppedProductBinding,
     private val navigateToProductDetail: (Int) -> Unit,
 ) : RecyclerView.ViewHolder(binding.root) {
     private var recommendJustDroppedAdapter: RecommendJustDroppedAdapter = RecommendJustDroppedAdapter(navigateToProductDetail)
+
     fun onBind(
         recommendJustDroppedProductModel: RecommendJustDroppedProductModel,
         position: Int,
     ) {
         with(binding) {
-            ivReleaseProduct.load(recommendJustDroppedProductModel.thumbnailUrl)
-            tvReleaseProductBrand.text = recommendJustDroppedProductModel.brandTitle
-            tvReleaseProductName.text = recommendJustDroppedProductModel.engTitle
-            tvReleaseProductPrice.text = recommendJustDroppedProductModel.price
-            tvReleaseProductTransaction.text = recommendJustDroppedProductModel.transactionCount
-            ivReleaseProductSaved.setImageResource(
-                if (recommendJustDroppedProductModel.isScrap) R.drawable.ic_saved_2_on_24
-                else R.drawable.ic_saved_2_off_24
+            ivJustDroppedProduct.load(recommendJustDroppedProductModel.thumbnailUrl)
+            tvJustDroppedProductBrand.text = recommendJustDroppedProductModel.brandTitle
+            tvJustDroppedProductName.text = recommendJustDroppedProductModel.engTitle
+            tvJustDroppedProductPrice.text = recommendJustDroppedProductModel.price
+            tvJustDroppedProductTransaction.text = recommendJustDroppedProductModel.transactionCount
+            ivJustDroppedProductScrap.setImageResource(
+                if (recommendJustDroppedProductModel.isScrap) {
+                    R.drawable.ic_saved_2_on_24
+                } else {
+                    R.drawable.ic_saved_2_off_24
+                },
             )
+            tvJustDroppedProductCoupon.visibility =
+                if (recommendJustDroppedProductModel.isCoupon) {
+                    View.VISIBLE
+                } else {
+                    View.GONE
+                }
+            tvJustDroppedProductSave.visibility =
+                if (recommendJustDroppedProductModel.isSave) {
+                    View.VISIBLE
+                } else {
+                    View.GONE
+                }
+            tvJustDroppedProductFreeDeliver.visibility =
+                if (recommendJustDroppedProductModel.isFreeDeliver) {
+                    View.VISIBLE
+                } else {
+                    View.INVISIBLE
+                }
         }
     }
 }
