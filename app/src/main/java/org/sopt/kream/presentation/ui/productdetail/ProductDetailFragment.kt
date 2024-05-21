2586a333947b1d6849d9671e981e1610cee46a52
jihyunniiii <jita1125@naver.com>
2024-05-22 03:54:54 +0900

    [feat] #26 상품 상세 뷰 상하단 바 구현

--- app/src/main/java/org/sopt/kream/presentation/ui/productdetail/ProductDetailFragment.kt
+++ app/src/main/java/org/sopt/kream/presentation/ui/productdetail/ProductDetailFragment.kt
@@ -1,6 +1,25 @@
 package org.sopt.kream.presentation.ui.productdetail
 
+import android.os.Bundle
+import android.view.View
 import org.sopt.kream.databinding.FragmentProductDetailBinding
+import org.sopt.kream.presentation.ui.type.ProductDetailButtonType
 import org.sopt.kream.util.base.BindingFragment
 
-class ProductDetailFragment : BindingFragment<FragmentProductDetailBinding>({ FragmentProductDetailBinding.inflate(it) })
+class ProductDetailFragment : BindingFragment<FragmentProductDetailBinding>({ FragmentProductDetailBinding.inflate(it) }) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initLayout()
+    }
+
+    private fun initLayout() {
+        with(binding) {
+            btnProductDetailBottomPurchase.setProductDetailButtonType(ProductDetailButtonType.PURCHASE)
+            btnProductDetailBottomSale.setProductDetailButtonType(ProductDetailButtonType.SALE)
+            btnProductDetailBottomSale.priceTextView.text = "148,000원"
+            tvProductDetailBottomScrap.text = "11.4만"
+            btnProductDetailBottomPurchase.priceTextView.text = "129,000원"
+        }
+    }
+}
