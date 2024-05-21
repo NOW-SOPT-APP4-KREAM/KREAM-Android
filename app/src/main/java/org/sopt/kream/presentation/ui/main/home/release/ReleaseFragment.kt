9da4b3200bef38fd8cc92ea09f98ca7647508ee9
t1nm1ksun <minsuk07672@naver.com>
2024-05-21 20:01:33 +0900

    Update ReleaseFragment.kt

--- app/src/main/java/org/sopt/kream/presentation/ui/main/home/release/ReleaseFragment.kt
+++ app/src/main/java/org/sopt/kream/presentation/ui/main/home/release/ReleaseFragment.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
