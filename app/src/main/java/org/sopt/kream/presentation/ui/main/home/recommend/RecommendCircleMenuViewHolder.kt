9de9011288dcaa1b209b150858a3e3acd1686bad
Hyobeen-Park <amanda0109@naver.com>
2024-05-24 01:29:09 +0900

    [add] #22 원형 메뉴 이미지 추가

--- app/src/main/java/org/sopt/kream/presentation/ui/main/home/recommend/RecommendCircleMenuViewHolder.kt
+++ app/src/main/java/org/sopt/kream/presentation/ui/main/home/recommend/RecommendCircleMenuViewHolder.kt
@@ -2,15 +2,17 @@ package org.sopt.kream.presentation.ui.main.home.recommend
 
 import androidx.recyclerview.widget.RecyclerView
 import org.sopt.kream.databinding.ItemRecommendCircleMenuBinding
+import org.sopt.kream.presentation.ui.type.RecommendCircleMenuType
 
 class RecommendCircleMenuViewHolder(
     private val binding: ItemRecommendCircleMenuBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
     fun onBind(
-        item: Int,
+        item: RecommendCircleMenuType,
     ) {
         with(binding) {
-            tvCircleMenuTitle.setText(item)
+            ivCircleMenu.setImageResource(item.image)
+            tvCircleMenuTitle.setText(item.menu)
         }
     }
 }
