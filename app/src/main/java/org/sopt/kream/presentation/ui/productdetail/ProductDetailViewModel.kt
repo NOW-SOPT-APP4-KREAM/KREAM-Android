adf2c383b4b5a72e57bed5d8474a18268c8c8a66
jihyunniiii <jita1125@naver.com>
2024-05-24 10:35:27 +0900

    [feat] #33 상품 상세 뷰 북마크

--- app/src/main/java/org/sopt/kream/presentation/ui/productdetail/ProductDetailViewModel.kt
+++ app/src/main/java/org/sopt/kream/presentation/ui/productdetail/ProductDetailViewModel.kt
@@ -15,6 +15,12 @@ class ProductDetailViewModel(
     private val _productDetailState = MutableStateFlow<UiState<ProductDetailModel>>(UiState.Empty)
     val productDetailState get() = _productDetailState.asStateFlow()
 
+    private val _postScrapState = MutableStateFlow<UiState<Int>>(UiState.Empty)
+    val postScrapState get() = _postScrapState.asStateFlow()
+
+    private val _deleteScrapState = MutableStateFlow<UiState<Int>>(UiState.Empty)
+    val deleteScrapState get() = _postScrapState.asStateFlow()
+
     fun getProductDetail(productId: Int) {
         viewModelScope.launch {
             _productDetailState.value = UiState.Loading
@@ -22,6 +28,28 @@ class ProductDetailViewModel(
                 _productDetailState.value = UiState.Success(productDetailModel)
             }.onFailure { exception: Throwable ->
                 _productDetailState.value = UiState.Error(exception.message)
+            }
+        }
+    }
+
+    fun postScrapProduct(productId: Int) {
+        viewModelScope.launch {
+            _postScrapState.value = UiState.Loading
+            productRepository.postScrap(productId = productId).onSuccess {
+                _postScrapState.value = UiState.Success(productId)
+            }.onFailure { exception: Throwable ->
+                _postScrapState.value = UiState.Error(exception.message)
+            }
+        }
+    }
+
+    fun deleteScrapProduct(productId: Int) {
+        viewModelScope.launch {
+            _deleteScrapState.value = UiState.Loading
+            productRepository.deleteScrap(productId = productId).onSuccess {
+                _deleteScrapState.value = UiState.Success(productId)
+            }.onFailure { exception: Throwable ->
+                _deleteScrapState.value = UiState.Error(exception.message)
             }
         }
     }
