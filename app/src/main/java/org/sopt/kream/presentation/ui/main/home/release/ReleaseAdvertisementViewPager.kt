d1bbd08a24e1f9e9752c4b95abaf5398b5cdd279
jihyunniiii <jita1125@naver.com>
2024-05-25 06:36:08 +0900

    [chore] #38 conflict 해결

--- app/src/main/java/org/sopt/kream/presentation/ui/main/home/release/ReleaseAdvertisementViewPager.kt
+++ app/src/main/java/org/sopt/kream/presentation/ui/main/home/release/ReleaseAdvertisementViewPager.kt
@@ -2,8 +2,10 @@ package org.sopt.kream.presentation.ui.main.home.release
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -12,6 +14,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.TabRowDefaults
+import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -20,6 +26,7 @@ import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.ExperimentalPagerApi
@@ -27,7 +34,7 @@ import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
 import kotlinx.coroutines.delay
 import org.sopt.kream.R
-import org.sopt.kream.presentation.ui.model.Advertisement
+import org.sopt.kream.presentation.model.Advertisement
 import org.sopt.kream.theme.robotoBold
 import java.util.concurrent.TimeUnit
 
